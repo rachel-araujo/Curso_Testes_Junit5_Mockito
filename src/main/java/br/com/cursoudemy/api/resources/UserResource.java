ed97dacc510dcc930e4e714e299b55c1e307998e
rachel-araujo <rachelaraujo17@gmail.com>
2023-01-11 18:59:01 -0300

    classe user resource findById

--- src/main/java/br/com/cursoudemy/api/resources/UserResource.java
+++ src/main/java/br/com/cursoudemy/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package br.com.cursoudemy.api.resources;
 
-import br.com.cursoudemy.api.domain.User;
+import br.com.cursoudemy.api.domain.Users;
+import br.com.cursoudemy.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,11 +10,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value ="/user")
+@RequestMapping(value ="/users")
 public class UserResource {
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
 
-        return ResponseEntity.ok().body(new User(1,"Rachel","email@gmail.com","123"));
+    @Autowired
+    private UserService service;
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Users> findById(@PathVariable Integer id) {
+
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
