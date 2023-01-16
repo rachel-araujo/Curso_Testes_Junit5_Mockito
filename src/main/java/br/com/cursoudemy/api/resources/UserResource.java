f91a77e8736b6623d528022e20f6e178e80dfc7d
rachel-araujo <rachelaraujo17@gmail.com>
2023-01-16 15:25:14 -0300

    endpoint findAll para users

--- src/main/java/br/com/cursoudemy/api/resources/UserResource.java
+++ src/main/java/br/com/cursoudemy/api/resources/UserResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value ="/users")
 public class UserResource {
@@ -20,5 +23,11 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UsersDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(service.findById(id), UsersDTO.class));
+    }
+    @GetMapping
+    public ResponseEntity<List<UsersDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(service.findAll()
+                        .stream().map(x->mapper.map(x,UsersDTO.class)).collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/cursoudemy/api/services/UserService.java
+++ src/main/java/br/com/cursoudemy/api/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.cursoudemy.api.services;
 
 import br.com.cursoudemy.api.domain.Users;
 
+import java.util.List;
+
 public interface UserService {
 
     Users findById(Integer id);
+    List<Users> findAll();
 }
