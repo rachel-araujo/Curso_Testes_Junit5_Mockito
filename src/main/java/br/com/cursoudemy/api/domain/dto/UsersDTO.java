a11bff8799dc64c3809e5b41ec779bc87d035cfd
rachel-araujo <rachelaraujo17@gmail.com>
2023-01-19 22:35:48 -0300

    removendo anotacoes nao utilizadas

--- src/main/java/br/com/cursoudemy/api/domain/dto/UsersDTO.java
+++ src/main/java/br/com/cursoudemy/api/domain/dto/UsersDTO.java
@@ -3,12 +3,10 @@ package br.com.cursoudemy.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UsersDTO {
 
     private Integer id;
